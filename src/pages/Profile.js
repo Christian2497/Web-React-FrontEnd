2b0f927c3828bb3e176c4f80974fb189f9b1c1d8
Marta Camacho <martacamachovargas@gmail.com>
2020-11-20 20:09:52 +0100

    profile in process

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { withAuth } from '../lib/AuthProvider';
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 class Profile extends Component {
-    
+    state = {
+        weightDifference: 0,
+    }
 
     componentDidMount = async () => {
         const user = await axios.get("http://localhost:4000")
@@ -11,16 +14,51 @@ class Profile extends Component {
         console.log(this.props.user, 'this is  the user')
     }
 
+    
+//   ESTOY INTENTANDO HACER LA DIFERENCIA ENTRE EL PESO Y EL OBJETIVO PERO NO ME SALE
+
+//     subtraction = (arr) => {
+//     if (Object.prototype.toString.call(arr) === '[object Array]') {
+//     var total = arr[0];
+//     if (typeof (total) !== 'number') {
+//       return false;
+//     }
+//     for (var i = 1, length = arr.length; i < length; i++){
+//       if (typeof (arr[i]) === 'number'){
+//         total -= arr[i];
+//       }else 
+//       return false;
+//     }
+//     return total;
+//    } 
+//     else
+//      return false;
+//   }
+    
+//   weightDifference = (weight, goal) => {
+//     weight = this.props.user.weight;
+//     goal = this.props.user.goal;
+//     const difference = weight - goal;
+//     this.setState({ weightDifference: difference})
+//     console.log(this.state.weightDifference, 'esta es la diferencia')
+// }
+  
+
 
     render() {
         
         return (
             <div>
-            <img src={this.props.user.imgPath} alt="user picture" />
+            <img src={this.props.user.imgPath} alt="user" />
+            <a href="/profile/${this.props.user._id}/edit"><img src="/images/edit-icon.svg" alt="pencil" /></a>
             <h1>Hello, {this.props.user.username}. Welcome to your profile.</h1>
-               <p>{this.props.user.weight}</p>
-               <p>{this.props.user.goal}</p>
-               
+               <p>Current weight: {this.props.user.weight}kg</p>
+               <p>Goal: {this.props.user.goal}kg</p>
+               <p>You have {this.weightDifference}kg left.</p>
+               <button><Link to={`/videos/favourites/${this.props.user._id}`} className="nav-link active">
+               Favourite videos
+                </Link></button>
+               <button><Link to='/profile/add-video' >Add new video</Link></button>
             </div>
         )
     }

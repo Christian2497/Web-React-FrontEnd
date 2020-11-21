7b39b2cc194db664e2b8e49c684695ee5cbccb8d
Christian <christian_8_neus@hotmail.com>
2020-11-21 15:31:38 +0100

    done

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { withAuth } from '../lib/AuthProvider';
 import axios from "axios";
+import { Link } from 'react-router-dom';
 
 class Profile extends Component {
     
@@ -16,6 +17,7 @@ class Profile extends Component {
         
         return (
             <div>
+            <Link to={`/profile/${this.props.user._id}/edit`}> Edit Profile </Link>
             <img src={this.props.user.imgPath} alt="user picture" />
             <h1>Hello, {this.props.user.username}. Welcome to your profile.</h1>
                <p>{this.props.user.weight}</p>

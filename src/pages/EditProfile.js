aee5e590c370e28ffe045abd98105ad3e31c2c2f
Christian <christian_8_neus@hotmail.com>
2020-11-26 12:03:22 +0100

    form css done

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -90,17 +90,17 @@ class EditProfile extends Component {
                 </div>
 
                 <div className="edit-weight">
-                    <label>Weight:</label>
+                    <label className="weight">Weight:</label>
                     <input type="number" name="weight" value={this.state.weight} onChange={ e => this.handleChangeWeight(e)} />
 
-                    <label>Goal:</label>
+                    <label className="weight">Goal:</label>
                     <input type="number" name="goal" value={this.state.goal} onChange={ e => this.handleChangeGoal(e)} />
                 </div>
-                <div className="input-imgPath">
+                <div className="div-photo">
                 <label  for="file-upload" className="custom-file-upload"> 
                   <i className="fa fa-cloud-upload"></i> Upload your photo
                 </label>
-                <input type="file" name="imgPath" onChange={ e => this.handleFileUpload(e)}/>
+                <input id="file-upload" type="file" name="imgPath" onChange={ e => this.handleFileUpload(e)}/>
                 </div>
                 <div className="edit-profile-button-div">
                   <input className="edit-profile-button" type="submit" value="Submit"/>
